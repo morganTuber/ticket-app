8b1056ab8c3a43c1a0e23a4a9c4faf23370dff6b
morganTuber <sachinaryal300@gmail.com>
2021-10-10 00:07:15 +0545

    fixed loading bug

--- src/components/helpers/ClientOnly.tsx
+++ src/components/helpers/ClientOnly.tsx
@@ -8,7 +8,7 @@ export const ClientOnly: FC = ({ children }): JSX.Element => {
     if (!isBrowser) {
         return (
             <div className='fixed inset-0 bg-white flex items-center justify-center'>
-                <div className='h-96 w-96 rounded-full bg-transparent border-8 border-purple-700 animate-spin'></div>
+                <div className='h-56 w-56 rounded-full bg-transparent border-8 border-purple-700 animate-spin'></div>
             </div>
         )
     }
